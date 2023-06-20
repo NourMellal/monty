4538d931a42c22d716520c4ca6b91bc51e5ee1ff
NourMellal <mellal.nour9@gmail.com>
2023-06-20 16:14:02 +0100

    getline() added to main.c

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 int main(int argc, char **argv)
 {
 	FILE *fp;
+	char *lineBuf = NULL, *token = NULL;
+	size_t num_ch_readed = 0;
+	uint line_counter = 1;
 
 	if (argc != 2)
 		error_arguments();
@@ -11,6 +14,13 @@ int main(int argc, char **argv)
 
 	if(fp == NULL)
 		open_error(argv);
+
+	/* get number of lines */
+	while(getline(&lineBuf, &num_ch_readed, fp) != EOF)
+	{
+		token = strtok(lineBuf, "\n");
+		line_counter++;
+	}
 
 	return (0);
 }
