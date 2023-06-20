a16431ef4fd8e1461ae55dca876205c400a71535
NourMellal <mellal.nour9@gmail.com>
2023-06-20 16:03:50 +0100

    monty invalid instruction error added to error handler

--- ErrorHandler.c
+++ ErrorHandler.c
@@ -13,3 +13,9 @@ void open_error(char **argv)
 	fprintf(stderr, "Error: can't open file %s\n", filename);
 	exit(EXIT_FAILURE);
 }
+
+void invalidInstruction_error(char *invalidInctruction, uint line)
+{
+	fprintf(stderr, "L%u: unknown instruction %s\n", line, invalidInctruction);
+	exit(EXIT_FAILURE);
+}
