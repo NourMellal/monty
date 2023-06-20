e0037032399fde264761308e1bd55290731c6ce5
NourMellal <mellal.nour9@gmail.com>
2023-06-20 16:26:35 +0100

    main.c : optimized adding open_and_read() function

--- main.c
+++ main.c
@@ -2,25 +2,6 @@
 
 int main(int argc, char **argv)
 {
-	FILE *fp;
-	char *lineBuf = NULL, *token = NULL;
-	size_t num_ch_readed = 0;
-	uint line_counter = 1;
-
-	if (argc != 2)
-		error_arguments();
-
-	fp = fopen(argv[1], "r");
-
-	if(fp == NULL)
-		open_error(argv);
-
-	/* get number of lines */
-	while(getline(&lineBuf, &num_ch_readed, fp) != EOF)
-	{
-		token = strtok(lineBuf, "\n");
-		line_counter++;
-	}
-
+	open_and_read(argv);
 	return (0);
 }
