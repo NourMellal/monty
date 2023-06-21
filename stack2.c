cf7023d3f8de7155a6b1fdbe210ee78da1ef78fe
NourMellal <mellal.nour9@gmail.com>
2023-06-21 15:51:53 +0100

    swap only number

--- stack2.c
+++ stack2.c
@@ -2,20 +2,12 @@
 
 void _swap(stack_t **top, uint line_number)
 {
-	stack_t *tmp, *two, *tree;
+	int num;
 
 	if (*top == NULL || (*top)->next == NULL)
 		swap_error(line_number);
 
-	tmp = *top;
-	two = tmp->next;
-	tree = tmp->next->next;
-
-	*top = two;
-	two->next = tmp;
-	two->prev = NULL;
-	tmp->prev = two;
-	tmp->next = tree;
-	tree->prev = tmp;
-
+	num = (*top)->n;
+	(*top)->n = (*top)->next->n;
+	(*top)->next->n = num;
 }
